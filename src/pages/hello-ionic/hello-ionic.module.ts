73d6de2f6b27986e376a43e360a1915cbf05791e
Yuri Miyazaki <yuritakeo@ucl.br>
2020-12-24 11:13:13 -0300

    Tela Inicial

--- src/pages/hello-ionic/hello-ionic.module.ts
+++ src/pages/hello-ionic/hello-ionic.module.ts
@@ -1,10 +1,11 @@
 import{ IonicPageModule } from 'ionic-angular/module';
 import { NgModule } from '@angular/core';
 import { HelloIonicPage } from './hello-ionic';
+
 @NgModule({
-declarations: [HelloIonicPage],
-imports: [IonicPageModule.forChild(HelloIonicPage)]
+    declarations: [HelloIonicPage],
+    imports: [IonicPageModule.forChild(HelloIonicPage)]
 })
 export class HomeModule{
-
+    
 }
